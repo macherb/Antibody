707404880c61c1ad96e8ce061620a549700ae090
Robert Macher <bobby_macher@hotmail.com>
2015-02-16 21:24:21 -0600

    More meaningful names to functions.
    Gave more meaningful names to two of the functions.

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,5 @@
-package com.example.antibody;
+package com.example.antibodyidentification;
 
-import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +25,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 						mV1,
 						mBigK1,
 						mLittleK1,
+						mKPA1,
+						mKPB1,
 
 						mD2,
 						mBigC2,
@@ -37,6 +38,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 						mV2,
 						mBigK2,
 						mLittleK2,
+						mKPA2,
+						mKPB2,
 
 						mD3,
 						mBigC3,
@@ -48,6 +51,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 						mV3,
 						mBigK3,
 						mLittleK3,
+						mKPA3,
+						mKPB3,
 
 						mD4,
 						mBigC4,
@@ -59,6 +64,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 						mV4,
 						mBigK4,
 						mLittleK4,
+						mKPA4,
+						mKPB4,
 
 						mD5,
 						mBigC5,
@@ -70,6 +77,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 						mV5,
 						mBigK5,
 						mLittleK5,
+						mKPA5,
+						mKPB5,
 
 						mD6,
 						mBigC6,
@@ -81,6 +90,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 						mV6,
 						mBigK6,
 						mLittleK6,
+						mKPA6,
+						mKPB6,
 
 						mSolution;
 
@@ -108,6 +119,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mV1 =			(TextView	)findViewById(R.id.textV1			);
 		mBigK1 =		(TextView	)findViewById(R.id.textBigK1		);
 		mLittleK1 =		(TextView	)findViewById(R.id.textLittleK1		);
+		mKPA1 =			(TextView	)findViewById(R.id.textKPA1			);
+		mKPB1 =			(TextView	)findViewById(R.id.textKPB1			);
 
 		mD2 =			(TextView	)findViewById(R.id.textD2			);
 		mBigC2 =		(TextView	)findViewById(R.id.textBigC2		);
@@ -119,6 +132,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mV2 =			(TextView	)findViewById(R.id.textV2			);
 		mBigK2 =		(TextView	)findViewById(R.id.textBigK2		);
 		mLittleK2 =		(TextView	)findViewById(R.id.textLittleK2		);
+		mKPA2 =			(TextView	)findViewById(R.id.textKPA2			);
+		mKPB2 =			(TextView	)findViewById(R.id.textKPB2			);
 
 		mD3 =			(TextView	)findViewById(R.id.textD3			);
 		mBigC3 =		(TextView	)findViewById(R.id.textBigC3		);
@@ -130,6 +145,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mV3 =			(TextView	)findViewById(R.id.textV3			);
 		mBigK3 =		(TextView	)findViewById(R.id.textBigK3		);
 		mLittleK3 =		(TextView	)findViewById(R.id.textLittleK3		);
+		mKPA3 =			(TextView	)findViewById(R.id.textKPA3			);
+		mKPB3 =			(TextView	)findViewById(R.id.textKPB3			);
 
 		mD4 =			(TextView	)findViewById(R.id.textD4			);
 		mBigC4 =		(TextView	)findViewById(R.id.textBigC4		);
@@ -141,6 +158,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mV4 =			(TextView	)findViewById(R.id.textV4			);
 		mBigK4 =		(TextView	)findViewById(R.id.textBigK4		);
 		mLittleK4 =		(TextView	)findViewById(R.id.textLittleK4		);
+		mKPA4 =			(TextView	)findViewById(R.id.textKPA4			);
+		mKPB4 =			(TextView	)findViewById(R.id.textKPB4			);
 
 		mD5 =			(TextView	)findViewById(R.id.textD5			);
 		mBigC5 =		(TextView	)findViewById(R.id.textBigC5		);
@@ -152,6 +171,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mV5 =			(TextView	)findViewById(R.id.textV5			);
 		mBigK5 =		(TextView	)findViewById(R.id.textBigK5		);
 		mLittleK5 =		(TextView	)findViewById(R.id.textLittleK5		);
+		mKPA5 =			(TextView	)findViewById(R.id.textKPA5			);
+		mKPB5 =			(TextView	)findViewById(R.id.textKPB5			);
 
 		mD6 =			(TextView	)findViewById(R.id.textD6			);
 		mBigC6 =		(TextView	)findViewById(R.id.textBigC6		);
@@ -163,9 +184,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mV6 =			(TextView	)findViewById(R.id.textV6			);
 		mBigK6 =		(TextView	)findViewById(R.id.textBigK6		);
 		mLittleK6 =		(TextView	)findViewById(R.id.textLittleK6		);
+		mKPA6 =			(TextView	)findViewById(R.id.textKPA6			);
+		mKPB6 =			(TextView	)findViewById(R.id.textKPB6			);
 
 		mSolution =		(TextView	)findViewById(R.id.textSolution		);
-		
+
 		mCheck1 =		(CheckBox	)findViewById(R.id.check1			);
 		mCheck2 =		(CheckBox	)findViewById(R.id.check2			);
 		mCheck3 =		(CheckBox	)findViewById(R.id.check3			);
@@ -176,8 +199,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		mCalculate =	(Button		)findViewById(R.id.buttonCalculate	);
         mCalculate.setOnClickListener(this);
 
-        antibodies =	new char[10];
-        
+        antibodies =	new char[12];
 	}
 
 	@Override
@@ -204,7 +226,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 		solutions =	0;
 
-		for (i = 0; i < 10; i++)
+		for (i = 0; i < 12; i++)
 			antibodies[i] =	0;
 
 		if      ( mCheck1.isChecked() &&  mCheck2.isChecked() &&  mCheck3.isChecked() &&  mCheck4.isChecked() &&  mCheck5.isChecked() &&  mCheck6.isChecked())
@@ -216,81 +238,93 @@ public class MainActivity extends Activity implements View.OnClickListener {
 			mSolution.setText("");
 			if (!mCheck1.isChecked())//Can use this row to cross out
 			{
-				CrossOut(	0, mD1		, 				1, "D");
-				CrossOut1(	1, mBigC1	, mLittleC1	,	1, "C");
-				CrossOut1(	2, mBigE1	, mLittleE1	,	1, "E");
-				CrossOut1(	3, mLittleC1, mBigC1	,	1, "c");
-				CrossOut1(	4, mLittleE1, mBigE1	,	1, "e");
-				CrossOut(	5, mF1		,				1, "f");
-				CrossOut(	6, mCW1		,				1, "Cw");
-				CrossOut(	7, mV1		,				1, "V");
-				CrossOut1(	8, mBigK1	, mLittleK1	,	1, "K");
-				CrossOut1(	9, mLittleK1, mBigK1	,	1, "k");
+				CrossOutSingle(	0, mD1		, 				1, "D");
+				CrossOutCouple(	1, mBigC1	, mLittleC1	,	1, "C");
+				CrossOutCouple(	2, mBigE1	, mLittleE1	,	1, "E");
+				CrossOutCouple(	3, mLittleC1, mBigC1	,	1, "c");
+				CrossOutCouple(	4, mLittleE1, mBigE1	,	1, "e");
+				CrossOutSingle(	5, mF1		,				1, "f");
+				CrossOutSingle(	6, mCW1		,				1, "Cw");
+				CrossOutSingle(	7, mV1		,				1, "V");
+				CrossOutCouple(	8, mBigK1	, mLittleK1	,	1, "K");
+				CrossOutCouple(	9, mLittleK1, mBigK1	,	1, "k");
+				CrossOutCouple(10, mKPA1	, mKPB1		,	1, "Kpa");
+				CrossOutCouple(11, mKPB1	, mKPA1		,	1, "Kpb");
 			}
 			if (!mCheck2.isChecked())//Can use this row to cross out
 			{
-				CrossOut(	0, mD2		, 				2, "D");
-				CrossOut1(	1, mBigC2	, mLittleC2	,	2, "C");
-				CrossOut1(	2, mBigE2	, mLittleE2	,	2, "E");
-				CrossOut1(	3, mLittleC2, mBigC2	,	2, "c");
-				CrossOut1(	4, mLittleE2, mBigE2	,	2, "e");
-				CrossOut(	5, mF2		,				2, "f");
-				CrossOut(	6, mCW2		,				2, "Cw");
-				CrossOut(	7, mV2		,				2, "V");
-				CrossOut1(	8, mBigK2	, mLittleK2	,	2, "K");
-				CrossOut1(	9, mLittleK2, mBigK2	,	2, "k");
+				CrossOutSingle(	0, mD2		, 				2, "D");
+				CrossOutCouple(	1, mBigC2	, mLittleC2	,	2, "C");
+				CrossOutCouple(	2, mBigE2	, mLittleE2	,	2, "E");
+				CrossOutCouple(	3, mLittleC2, mBigC2	,	2, "c");
+				CrossOutCouple(	4, mLittleE2, mBigE2	,	2, "e");
+				CrossOutSingle(	5, mF2		,				2, "f");
+				CrossOutSingle(	6, mCW2		,				2, "Cw");
+				CrossOutSingle(	7, mV2		,				2, "V");
+				CrossOutCouple(	8, mBigK2	, mLittleK2	,	2, "K");
+				CrossOutCouple(	9, mLittleK2, mBigK2	,	2, "k");
+				CrossOutCouple(10, mKPA2	, mKPB2		,	2, "Kpa");
+				CrossOutCouple(11, mKPB2	, mKPA2		,	2, "Kpb");
 			}
 			if (!mCheck3.isChecked())//Can use this row to cross out
 			{
-				CrossOut(	0, mD3		,				3, "D");
-				CrossOut1(	1, mBigC3	, mLittleC3	,	3, "C");
-				CrossOut1(	2, mBigE3	, mLittleE3	,	3, "E");
-				CrossOut1(	3, mLittleC3, mBigC3	,	3, "c");
-				CrossOut1(	4, mLittleE3, mBigE3	,	3, "e");
-				CrossOut(	5, mF3		,				3, "f");
-				CrossOut(	6, mCW3		,				3, "Cw");
-				CrossOut(	7, mV3		,				3, "V");
-				CrossOut1(	8, mBigK3	, mLittleK3	,	3, "K");
-				CrossOut1(	9, mLittleK3, mBigK3	,	3, "k");
+				CrossOutSingle(	0, mD3		,				3, "D");
+				CrossOutCouple(	1, mBigC3	, mLittleC3	,	3, "C");
+				CrossOutCouple(	2, mBigE3	, mLittleE3	,	3, "E");
+				CrossOutCouple(	3, mLittleC3, mBigC3	,	3, "c");
+				CrossOutCouple(	4, mLittleE3, mBigE3	,	3, "e");
+				CrossOutSingle(	5, mF3		,				3, "f");
+				CrossOutSingle(	6, mCW3		,				3, "Cw");
+				CrossOutSingle(	7, mV3		,				3, "V");
+				CrossOutCouple(	8, mBigK3	, mLittleK3	,	3, "K");
+				CrossOutCouple(	9, mLittleK3, mBigK3	,	3, "k");
+				CrossOutCouple(10, mKPA3	, mKPB3		,	3, "Kpa");
+				CrossOutCouple(11, mKPB3	, mKPA3		,	3, "Kpb");
 			}
 			if (!mCheck4.isChecked())//Can use this row to cross out
 			{
-				CrossOut(	0, mD4		,				4, "D");
-				CrossOut1(	1, mBigC4	, mLittleC4	,	4, "C");
-				CrossOut1(	2, mBigE4	, mLittleE4	,	4, "E");
-				CrossOut1(	3, mLittleC4, mBigC4	,	4, "c");
-				CrossOut1(	4, mLittleE4, mBigE4	,	4, "e");
-				CrossOut(	5, mF4		,				4, "f");
-				CrossOut(	6, mCW4		,				4, "Cw");
-				CrossOut(	7, mV4		,				4, "V");
-				CrossOut1(	8, mBigK4	, mLittleK4	,	4, "K");
-				CrossOut1(	9, mLittleK4, mBigK4	,	4, "k");
+				CrossOutSingle(	0, mD4		,				4, "D");
+				CrossOutCouple(	1, mBigC4	, mLittleC4	,	4, "C");
+				CrossOutCouple(	2, mBigE4	, mLittleE4	,	4, "E");
+				CrossOutCouple(	3, mLittleC4, mBigC4	,	4, "c");
+				CrossOutCouple(	4, mLittleE4, mBigE4	,	4, "e");
+				CrossOutSingle(	5, mF4		,				4, "f");
+				CrossOutSingle(	6, mCW4		,				4, "Cw");
+				CrossOutSingle(	7, mV4		,				4, "V");
+				CrossOutCouple(	8, mBigK4	, mLittleK4	,	4, "K");
+				CrossOutCouple(	9, mLittleK4, mBigK4	,	4, "k");
+				CrossOutCouple(10, mKPA4	, mKPB4		,	4, "Kpa");
+				CrossOutCouple(11, mKPB4	, mKPA4		,	4, "Kpb");
 			}
 			if (!mCheck5.isChecked())//Can use this row to cross out
 			{
-				CrossOut(	0, mD5		,				5, "D");
-				CrossOut1(	1, mBigC5	, mLittleC5	,	5, "C");
-				CrossOut1(	2, mBigE5	, mLittleE5	,	5, "E");
-				CrossOut1(	3, mLittleC5, mBigC5	,	4, "c");
-				CrossOut1(	4, mLittleE5, mBigE5	,	5, "e");
-				CrossOut(	5, mF5		,				5, "f");
-				CrossOut(	6, mCW5		,				5, "Cw");
-				CrossOut(	7, mV5		,				5, "V");
-				CrossOut1(	8, mBigK5	, mLittleK5	,	5, "K");
-				CrossOut1(	9, mLittleK5, mBigK5	,	5, "k");
+				CrossOutSingle(	0, mD5		,				5, "D");
+				CrossOutCouple(	1, mBigC5	, mLittleC5	,	5, "C");
+				CrossOutCouple(	2, mBigE5	, mLittleE5	,	5, "E");
+				CrossOutCouple(	3, mLittleC5, mBigC5	,	4, "c");
+				CrossOutCouple(	4, mLittleE5, mBigE5	,	5, "e");
+				CrossOutSingle(	5, mF5		,				5, "f");
+				CrossOutSingle(	6, mCW5		,				5, "Cw");
+				CrossOutSingle(	7, mV5		,				5, "V");
+				CrossOutCouple(	8, mBigK5	, mLittleK5	,	5, "K");
+				CrossOutCouple(	9, mLittleK5, mBigK5	,	5, "k");
+				CrossOutCouple(10, mKPA5	, mKPB5		,	5, "Kpa");
+				CrossOutCouple(11, mKPB5	, mKPA5		,	5, "Kpb");
 			}
 			if (!mCheck6.isChecked())//Can use this row to cross out
 			{
-				CrossOut(	0, mD6		,				6, "D");
-				CrossOut1(	1, mBigC6	, mLittleC6	,	6, "C");
-				CrossOut1(	2, mBigE6	, mLittleE6	,	6, "E");
-				CrossOut1(	3, mLittleC6, mBigC6	,	6, "c");
-				CrossOut1(	4, mLittleE6, mBigE6	,	6, "e");
-				CrossOut(	5, mF6		,				6, "f");
-				CrossOut(	6, mCW6		,				6, "Cw");
-				CrossOut(	7, mV6		,				6, "V");
-				CrossOut1(	8, mBigK6	, mLittleK6	,	6, "K");
-				CrossOut1(	9, mLittleK6, mBigK6	,	6, "k");
+				CrossOutSingle(	0, mD6		,				6, "D");
+				CrossOutCouple(	1, mBigC6	, mLittleC6	,	6, "C");
+				CrossOutCouple(	2, mBigE6	, mLittleE6	,	6, "E");
+				CrossOutCouple(	3, mLittleC6, mBigC6	,	6, "c");
+				CrossOutCouple(	4, mLittleE6, mBigE6	,	6, "e");
+				CrossOutSingle(	5, mF6		,				6, "f");
+				CrossOutSingle(	6, mCW6		,				6, "Cw");
+				CrossOutSingle(	7, mV6		,				6, "V");
+				CrossOutCouple(	8, mBigK6	, mLittleK6	,	6, "K");
+				CrossOutCouple(	9, mLittleK6, mBigK6	,	6, "k");
+				CrossOutCouple(10, mKPA6	, mKPB6		,	6, "Kpa");
+				CrossOutCouple(11, mKPB6	, mKPA6		,	6, "Kpb");
 			}
 			Use1ToGetSolutions();
 			Use2ToGetSolutions();
@@ -301,7 +335,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		}
 	}
 
-	void CrossOut(int index, TextView Antibody, int cell, String letter)
+	void CrossOutSingle(int index, TextView Antibody, int cell, String letter)
 	{
 		if (
 			(antibodies[index] < 255)
@@ -314,7 +348,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		}
 	}
 
-	void CrossOut1(int index, TextView Big, TextView Little, int cell, String letter)
+	void CrossOutCouple(int index, TextView Big, TextView Little, int cell, String letter)
 	{
 		if (
 			(antibodies[index] < 255)
@@ -332,16 +366,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (mCheck1.isChecked())
 		{
 			Log.d("Antibody", "Using 1 to get solutions");
-			GetSolution(0, mD1		, "D");
-			GetSolution(1, mBigC1	, "C");
-			GetSolution(2, mBigE1	, "E");
-			GetSolution(3, mLittleC1, "c");
-			GetSolution(4, mLittleE1, "e");
-			GetSolution(5, mF1		, "f");
-			GetSolution(6, mCW1		, "Cw");
-			GetSolution(7, mV1		, "V");
-			GetSolution(8, mBigK1	, "K");
-			GetSolution(9, mLittleK1, "k");
+			GetSolution( 0, mD1			, "D");
+			GetSolution( 1, mBigC1		, "C");
+			GetSolution( 2, mBigE1		, "E");
+			GetSolution( 3, mLittleC1	, "c");
+			GetSolution( 4, mLittleE1	, "e");
+			GetSolution( 5, mF1			, "f");
+			GetSolution( 6, mCW1		, "Cw");
+			GetSolution( 7, mV1			, "V");
+			GetSolution( 8, mBigK1		, "K");
+			GetSolution( 9, mLittleK1	, "k");
+			GetSolution(10, mKPA1		, "Kpa");
+			GetSolution(11, mKPB1		, "Kpb");
 		}
 	}
 
@@ -350,16 +386,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (mCheck2.isChecked())
 		{
 			Log.d("Antibody", "Using 2 to get solutions");
-			GetSolution(0, mD2		, "D");
-			GetSolution(1, mBigC2	, "C");
-			GetSolution(2, mBigE2	, "E");
-			GetSolution(3, mLittleC2, "c");
-			GetSolution(4, mLittleE2, "e");
-			GetSolution(5, mF2		, "f");
-			GetSolution(6, mCW2		, "Cw");
-			GetSolution(7, mV2		, "V");
-			GetSolution(8, mBigK2	, "K");
-			GetSolution(9, mLittleK2, "k");
+			GetSolution( 0, mD2			, "D");
+			GetSolution( 1, mBigC2		, "C");
+			GetSolution( 2, mBigE2		, "E");
+			GetSolution( 3, mLittleC2	, "c");
+			GetSolution( 4, mLittleE2	, "e");
+			GetSolution( 5, mF2			, "f");
+			GetSolution( 6, mCW2		, "Cw");
+			GetSolution( 7, mV2			, "V");
+			GetSolution( 8, mBigK2		, "K");
+			GetSolution( 9, mLittleK2	, "k");
+			GetSolution(10, mKPA2		, "Kpa");
+			GetSolution(11, mKPB2		, "Kpb");
 		}
 	}
 
@@ -368,16 +406,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (mCheck3.isChecked())
 		{
 			Log.d("Antibody", "Using 3 to get solutions");
-			GetSolution(0, mD3		, "D");
-			GetSolution(1, mBigC3	, "C");
-			GetSolution(2, mBigE3	, "E");
-			GetSolution(3, mLittleC3, "c");
-			GetSolution(4, mLittleE3, "e");
-			GetSolution(5, mF3		, "f");
-			GetSolution(6, mCW3		, "Cw");
-			GetSolution(7, mV3		, "V");
-			GetSolution(8, mBigK3	, "K");
-			GetSolution(9, mLittleK3, "k");
+			GetSolution( 0, mD3			, "D");
+			GetSolution( 1, mBigC3		, "C");
+			GetSolution( 2, mBigE3		, "E");
+			GetSolution( 3, mLittleC3	, "c");
+			GetSolution( 4, mLittleE3	, "e");
+			GetSolution( 5, mF3			, "f");
+			GetSolution( 6, mCW3		, "Cw");
+			GetSolution( 7, mV3			, "V");
+			GetSolution( 8, mBigK3		, "K");
+			GetSolution( 9, mLittleK3	, "k");
+			GetSolution(10, mKPA3		, "Kpa");
+			GetSolution(11, mKPB3		, "Kpb");
 		}
 	}
 
@@ -386,16 +426,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (mCheck4.isChecked())
 		{
 			Log.d("Antibody", "Using 4 to get solutions");
-			GetSolution(0, mD4		, "D");
-			GetSolution(1, mBigC4	, "C");
-			GetSolution(2, mBigE4	, "E");
-			GetSolution(3, mLittleC4, "c");
-			GetSolution(4, mLittleE4, "e");
-			GetSolution(5, mF4		, "f");
-			GetSolution(6, mCW4		, "Cw");
-			GetSolution(7, mV4		, "V");
-			GetSolution(8, mBigK4	, "K");
-			GetSolution(9, mLittleK4, "k");
+			GetSolution( 0, mD4			, "D");
+			GetSolution( 1, mBigC4		, "C");
+			GetSolution( 2, mBigE4		, "E");
+			GetSolution( 3, mLittleC4	, "c");
+			GetSolution( 4, mLittleE4	, "e");
+			GetSolution( 5, mF4			, "f");
+			GetSolution( 6, mCW4		, "Cw");
+			GetSolution( 7, mV4			, "V");
+			GetSolution( 8, mBigK4		, "K");
+			GetSolution( 9, mLittleK4	, "k");
+			GetSolution(10, mKPA4		, "Kpa");
+			GetSolution(11, mKPB4		, "Kpb");
 		}
 	}
 
@@ -404,16 +446,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (mCheck5.isChecked())
 		{
 			Log.d("Antibody", "Using 5 to get solutions");
-			GetSolution(0, mD5		, "D");
-			GetSolution(1, mBigC5	, "C");
-			GetSolution(2, mBigE5	, "E");
-			GetSolution(3, mLittleC5, "c");
-			GetSolution(4, mLittleE5, "e");
-			GetSolution(5, mF5		, "f");
-			GetSolution(6, mCW5		, "Cw");
-			GetSolution(7, mV5		, "V");
-			GetSolution(8, mBigK5	, "K");
-			GetSolution(9, mLittleK5, "k");
+			GetSolution( 0, mD5			, "D");
+			GetSolution( 1, mBigC5		, "C");
+			GetSolution( 2, mBigE5		, "E");
+			GetSolution( 3, mLittleC5	, "c");
+			GetSolution( 4, mLittleE5	, "e");
+			GetSolution( 5, mF5			, "f");
+			GetSolution( 6, mCW5		, "Cw");
+			GetSolution( 7, mV5			, "V");
+			GetSolution( 8, mBigK5		, "K");
+			GetSolution( 9, mLittleK5	, "k");
+			GetSolution(10, mKPA5		, "Kpa");
+			GetSolution(11, mKPB5		, "Kpb");
 		}
 	}
 
@@ -422,16 +466,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (mCheck6.isChecked())
 		{
 			Log.d("Antibody", "Using 6 to get solutions");
-			GetSolution(0, mD6		, "D");
-			GetSolution(1, mBigC6	, "C");
-			GetSolution(2, mBigE6	, "E");
-			GetSolution(3, mLittleC6, "c");
-			GetSolution(4, mLittleE6, "e");
-			GetSolution(5, mF6		, "f");
-			GetSolution(6, mCW6		, "Cw");
-			GetSolution(7, mV6		, "V");
-			GetSolution(8, mBigK6	, "K");
-			GetSolution(9, mLittleK6, "k");
+			GetSolution( 0, mD6			, "D");
+			GetSolution( 1, mBigC6		, "C");
+			GetSolution( 2, mBigE6		, "E");
+			GetSolution( 3, mLittleC6	, "c");
+			GetSolution( 4, mLittleE6	, "e");
+			GetSolution( 5, mF6			, "f");
+			GetSolution( 6, mCW6		, "Cw");
+			GetSolution( 7, mV6			, "V");
+			GetSolution( 8, mBigK6		, "K");
+			GetSolution( 9, mLittleK6	, "k");
+			GetSolution(10, mKPA6		, "Kpa");
+			GetSolution(11, mKPB6		, "Kpb");
 		}
 	}
 
